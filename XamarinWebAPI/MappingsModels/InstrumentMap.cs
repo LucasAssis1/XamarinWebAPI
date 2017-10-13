3a9a60be18f593407c9fc032cf836fdb861d28cd
Cleiton Barbieri <cleiton.barbieri@programmers.com.br>
2017-10-13 16:14:42 -0300

    Mapping new classes NHibernate V.2

---
+++ XamarinWebAPI/MappingsModels/InstrumentMap.cs
@@ -0,0 +1,19 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using XamarinWebAPI.Models;
+
+namespace XamarinWebAPI.MappingsModels
+{
+    public class InstrumentMap : ClassMap<InstrumentModel>
+    {
+        public InstrumentMap()
+        {
+            Id(x=>x.ID).Not.Nullable();
+            Map(x => x.About).Length(20).Not.Nullable();
+            Table("INSTRUMENTO");
+        }
+    }
+}
